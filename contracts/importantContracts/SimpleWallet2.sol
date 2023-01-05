cc809f58dd8caa0524d9463b8a3ec50cd9ba432a
Siddharth-sing <1906211@kiit.ac.in>
2023-01-05 18:34:23 +0530

    🌟Simple wallet 2 completed

--- contracts/importantContracts/SimpleWallet2.sol
+++ contracts/importantContracts/SimpleWallet2.sol
@@ -18,6 +18,16 @@ contract Simplewallet2{
     
     mapping(address => BalanceDetailsStruct) public balanceRecieved;
 
+    function showThisDepositDetails(address _from, uint _depositNum) public view returns(Transaction memory){
+        return balanceRecieved[_from].deposites[_depositNum];
+    }
+    function showThisWithdrawalDetails(address _from, uint _withdrawalNum) public view returns(Transaction memory){
+        return balanceRecieved[_from].withdrawals[_withdrawalNum];
+    }
+    
+
+    // Deposit or send money tosmart contract
+
     function depositeMoneyToSmartContract() public payable{
 
         Transaction memory thisTransactionDeposite = Transaction(msg.value,block.timestamp);
@@ -27,21 +37,19 @@ contract Simplewallet2{
         balanceRecieved[msg.sender].deposites[balanceRecieved[msg.sender].numsDeposites]=thisTransactionDeposite;
 
     }
+
+    // Withdwawal money from contract
+
     function withdrawFromContractWallet(address payable _to, uint _amount) public{
         
-        if(_amount > balanceRecieved[msg.sender].totalBalance){
-             Transaction memory thisWithdrawalTransaction = Transaction(balanceRecieved[msg.sender].totalBalance,block.timestamp);
-             balanceRecieved[msg.sender].numsWithdrawals++;
-             balanceRecieved[msg.sender].withdrawals[balanceRecieved[msg.sender].numsWithdrawals] = thisWithdrawalTransaction;
-             balanceRecieved[msg.sender].totalBalance = 0;
-             _to.transfer(_amount);
-
-        }else{
+        if(_amount < balanceRecieved[msg.sender].totalBalance){
             Transaction memory thisWithdrawalTransaction = Transaction(_amount,block.timestamp);
             balanceRecieved[msg.sender].numsWithdrawals++;
             balanceRecieved[msg.sender].withdrawals[balanceRecieved[msg.sender].numsWithdrawals] = thisWithdrawalTransaction;
             balanceRecieved[msg.sender].totalBalance -= _amount;
             _to.transfer(_amount);
+        }else{
+           
         }
         
         
@@ -50,12 +58,12 @@ contract Simplewallet2{
     }
 
     
-    //utility functions (not neccessary for this contract) 
-    function getAccountBalance(address ContractAddress) public view returns(uint){
-        return ContractAddress.balance;  //ContractAddress.balance will return the balance of the account. 
-    }
-     function getBalanceofSmartContract() public view returns(uint){
-        return(address(this).balance);  // address(this).balance will return the balance of smart contract 
-    }
+    // //utility functions (not neccessary for this contract) 
+    // function getAccountBalance(address ContractAddress) public view returns(uint){
+    //     return ContractAddress.balance;  //ContractAddress.balance will return the balance of the account. 
+    // }
+    //  function getBalanceofSmartContract() public view returns(uint){
+    //     return(address(this).balance);  // address(this).balance will return the balance of smart contract 
+    // }
 
 }
