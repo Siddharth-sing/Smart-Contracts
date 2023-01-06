73fc98494ab93b73720ddfc807df0264bf490bcb
Siddharth-sing <1906211@kiit.ac.in>
2023-01-06 13:58:18 +0530

    🤯 Try-Catch Solidity

--- contracts/ExampleTryCatch.sol
+++ contracts/ExampleTryCatch.sol
@@ -1,8 +1,32 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-contract ExampleTryCatch{
+contract WillThrowException{
 
-    
+    function ProblematicFunc1() public pure{
+        require(false,"Error Message");
+    }
+    function ProblematicFunc2() public pure{
+        assert(false);
+    }
+}
 
+contract ErrorHandling{
+    event ErrorLog(string reason);
+    event ErrorLog2(uint eCode);
+
+    function catchError() public {
+        WillThrowException e = new WillThrowException();
+        try e.ProblematicFunc1(){
+            // it works, further code
+        }catch Error(string memory reason){  // `catch Error` is used to catch exceptions caused by require
+            emit ErrorLog(reason);
+        }
+        
+        try e.ProblematicFunc2(){
+            // it works, further code
+        }catch Panic(uint errorCode){  // `catch Panic` is used to catch exceptions caused by assert.
+            emit ErrorLog2(errorCode);
+        } 
+    }
 }
