cbf0c875403c0c34d2563addf4d13419cb7b18af
Siddharth-sing <1906211@kiit.ac.in>
2023-01-24 16:40:55 +0530

    ✅Modifier and inheritance in solidity

--- contracts/SampleEvents.sol
+++ contracts/SampleEvents.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.16;
 contract SampleEvents{
 
     mapping(address => uint) public tokenBalance;
-
+    event TokenSent(address _from, address _to, uint _amount);
     constructor(){
         tokenBalance[msg.sender] = 100;
     }
@@ -13,7 +13,8 @@ contract SampleEvents{
         require(tokenBalance[msg.sender] >= _amount, "Not enough balance!");
         tokenBalance[msg.sender] -= _amount;
         tokenBalance[_to] += _amount;
-        return true;
+        emit TokenSent(msg.sender, _to, _amount);
+        return true; 
     }
 
 }
